fb5179e03f787b0f3b2b91815d7e5aa121e99e67
Jerome St-Louis <jerome@ecere.com>
2025-04-05 12:36:49 -0400

    rust bindings: unsafe { } use only inside bindings

--- bindings_examples/rust/info.rs
+++ bindings_examples/rust/info.rs
@@ -15,7 +15,6 @@ use std::collections::HashMap;
 
 // *** This code should be moved to / generated inside the DGGAL rust bindings
 const nullZone : dggal::DGGRSZone = 0xFFFFFFFFFFFFFFFFu64;
-const nullCStr : * const i8 = 0 as * const i8;
 const nullInst : dggal::Instance = 0 as dggal::Instance;
 const nullPtr : *mut *mut c_void = 0 as *mut *mut c_void;
 
@@ -78,7 +77,7 @@ impl DGGAL {
 impl DGGRS {
    // TODO: Could we use rust function-generating macros?
 
-   fn getZoneFromTextID(&self, zoneID: * const i8) -> dggal::DGGRSZone
+   fn getZoneFromTextID(&self, zoneID: &str) -> dggal::DGGRSZone
    {
       let mut zone = nullZone;
       unsafe
@@ -88,7 +87,8 @@ impl DGGRS {
          let cMethod: usize = std::mem::transmute(*vTbl.add(dggal::DGGRS_getZoneFromTextID_vTblID as usize));
          if cMethod != 0usize {
             let method : unsafe extern "C" fn(dggrs: dggal::DGGRS, zoneID: * const i8) -> dggal::DGGRSZone = std::mem::transmute(cMethod);
-            zone = method(self.imp, zoneID);
+            let csZoneID = CString::new(zoneID).unwrap();
+            zone = method(self.imp, csZoneID.as_ptr());
          }
       }
       zone
@@ -421,7 +421,7 @@ fn zoneInfo(dggrs: DGGRS, zone: dggal::DGGRSZone, options: HashMap<&str, &str>) 
    let centroidParent: dggal::DGGRSZone = dggrs.getZoneCentroidParent(zone);
    let centroidChild: dggal::DGGRSZone = dggrs.getZoneCentroidChild(zone);
    let isCentroidChild = dggrs.isZoneCentroidChild(zone);
-   let crs = (c"EPSG:4326").as_ptr();
+   let crs = &"EPSG:4326";
    let depthOption = options.get(&"depth");
 
    if depthOption != None
@@ -504,9 +504,8 @@ fn zoneInfo(dggrs: DGGRS, zone: dggal::DGGRSZone, options: HashMap<&str, &str>) 
       i += 1;
    }
    println!("");
-   let sCRS = unsafe { CStr::from_ptr(crs).to_str().unwrap() };
    let nVertices = vertices.len();
-   println!("[{sCRS}] Vertices ({nVertices}):");
+   println!("[{crs}] Vertices ({nVertices}):");
 
    for v in vertices
    {
@@ -541,92 +540,83 @@ fn displayInfo(dggrs: DGGRS, zone: dggal::DGGRSZone, options: HashMap<&str, &str
 
 fn main()
 {
-   unsafe
+   let args: Vec<String> = env::args().collect();
+   let argc = args.len();
+   let myApp = Application::new(&args);
+   let dggal = DGGAL::new(&myApp);
+   let mut exitCode: i32 = 0;
+   let mut show_syntax = false;
+   let mut dggrsName = "";
+   let mut a : usize = 1;
+   let mut zoneID: &str = "";
+   let arg0: &str = &args[0];
+   let mut options = HashMap::<&str, &str>::new();
+
+        if arg0.eq_ignore_ascii_case("i3h") { dggrsName = &"ISEA3H"; }
+   else if arg0.eq_ignore_ascii_case("i9r") { dggrsName = &"ISEA9R"; }
+   else if arg0.eq_ignore_ascii_case("ggg") { dggrsName = &"GNOSISGlobalGrid"; }
+
+   if dggrsName == "" && argc > 1
    {
-      let args: Vec<String> = env::args().collect();
-      let argc = args.len();
-      let myApp = Application::new(&args);
-      let dggal = DGGAL::new(&myApp);
-      let mut exitCode: i32 = 0;
-      let mut show_syntax = false;
-      let mut dggrsName: *const i8 = nullCStr;
-      let mut a : usize = 1;
-      let mut zoneID: *const i8 = nullCStr;
-      let arg0CStr = CString::new(args[0].clone()).unwrap();
-      let arg0 : *const i8 = arg0CStr.as_ptr();
-      let mut options = HashMap::<&str, &str>::new();
+      let arg1: &str = &args[1];
+           if arg1.eq_ignore_ascii_case("isea3h") { dggrsName = &"ISEA3H"; }
+      else if arg1.eq_ignore_ascii_case("isea9r") { dggrsName = &"ISEA9R"; }
+      else if arg1.eq_ignore_ascii_case("gnosis") { dggrsName = &"GNOSISGlobalGrid"; }
+      a += 1;
+   }
 
-           if 0 == dggal::strcasecmp(arg0, (c"i3h").as_ptr()) { dggrsName = (c"ISEA3H").as_ptr(); }
-      else if 0 == dggal::strcasecmp(arg0, (c"i9r").as_ptr()) { dggrsName = (c"ISEA9R").as_ptr(); }
-      else if 0 == dggal::strcasecmp(arg0, (c"ggg").as_ptr()) { dggrsName = (c"GNOSISGlobalGrid").as_ptr(); }
+   if argc > a {
+      zoneID = &args[a];
+      a+=1;
+   }
 
-      if dggrsName == nullCStr && argc > 1
+   while a < argc
+   {
+      let key = &args[a];
+      a+=1;
+      if key.as_bytes()[0] == '-' as u8 && a < argc
       {
-         let arg1cs = CString::new(args[1].clone()).unwrap(); // This variable needs to stay alive or the pointer will point to garbage
-         let arg1 : *const i8 = arg1cs.as_ptr();
-              if 0 == dggal::strcasecmp(arg1, (c"isea3h").as_ptr()) { dggrsName = (c"ISEA3H").as_ptr(); }
-         else if 0 == dggal::strcasecmp(arg1, (c"isea9r").as_ptr()) { dggrsName = (c"ISEA9R").as_ptr(); }
-         else if 0 == dggal::strcasecmp(arg1, (c"gnosis").as_ptr()) { dggrsName = (c"GNOSISGlobalGrid").as_ptr(); }
-         a += 1;
-      }
-
-      let zoneArg;
-      if argc > a {
-         zoneArg = CString::new(args[a].clone()).unwrap();
-         zoneID = zoneArg.as_ptr();
+         options.insert(&key[1..], &args[a]);
          a+=1;
-      }
-
-      while a < argc
-      {
-         let key = &args[a];
-         a+=1;
-         if key.as_bytes()[0] == '-' as u8 && a < argc
-         {
-            options.insert(&key[1..], &args[a]);
-            a+=1;
-         }
-         else
-         {
-            exitCode = 1;
-            show_syntax = true;
-         }
-      }
-
-      if dggrsName != nullCStr && exitCode == 0
-      {
-         let dn = CStr::from_ptr(dggrsName).to_str().unwrap();
-         let dggrs: DGGRS = dggal.newDGGRS(dn).expect("Unknown DGGRS");
-         let mut zone = nullZone;
-
-         println!("DGGRS: https://maps.gnosis.earth/ogcapi/dggrs/{dn}");
-
-         if zoneID != nullCStr {
-            zone = dggrs.getZoneFromTextID(zoneID);
-            if zone == nullZone
-            {
-               let za = CStr::from_ptr(zoneID).to_str().unwrap();
-               println!("Invalid {dn} zone identifier: {za}");
-               exitCode = 1;
-            }
-         }
-
-         if exitCode == 0 {
-            displayInfo(dggrs, zone, options);
-         }
       }
       else
       {
-         show_syntax = true;
          exitCode = 1;
+         show_syntax = true;
       }
-
-      if show_syntax {
-         println!("Syntax:");
-         println!("   info <dggrs> [zone] [options]");
-         println!("where dggrs is one of gnosis, isea3h or isea9r");
-      }
-
-      std::process::exit(exitCode)
    }
+
+   if dggrsName != "" && exitCode == 0
+   {
+      let dggrs: DGGRS = dggal.newDGGRS(dggrsName).expect("Unknown DGGRS");
+      let mut zone = nullZone;
+
+      println!("DGGRS: https://maps.gnosis.earth/ogcapi/dggrs/{dggrsName}");
+
+      if zoneID != "" {
+         zone = dggrs.getZoneFromTextID(zoneID);
+         if zone == nullZone
+         {
+            println!("Invalid {dggrsName} zone identifier: {zoneID}");
+            exitCode = 1;
+         }
+      }
+
+      if exitCode == 0 {
+         displayInfo(dggrs, zone, options);
+      }
+   }
+   else
+   {
+      show_syntax = true;
+      exitCode = 1;
+   }
+
+   if show_syntax {
+      println!("Syntax:");
+      println!("   info <dggrs> [zone] [options]");
+      println!("where dggrs is one of gnosis, isea3h or isea9r");
+   }
+
+   std::process::exit(exitCode)
 }
